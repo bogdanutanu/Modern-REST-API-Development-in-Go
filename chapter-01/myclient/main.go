237013b6d8ce95b12987465cb0e55f6a196b4d93
Jesús Espino <jesus@gitpod.io>
2025-07-13 10:35:03 +0200

    Initial commit

---
+++ chapter-01/myclient/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func main() {
+	result, err := http.Get("http://localhost:8888/")
+	if err != nil {
+		panic(err)
+	}
+	defer result.Body.Close()
+	data, err := io.ReadAll(result.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Response:", string(data))
+}
