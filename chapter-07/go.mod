66b8e7fb00a124e222116a22107b93b89b84f99a
Jesús Espino <jesus@gitpod.io>
2025-07-13 11:30:04 +0200

    Adding chapter 7

---
+++ chapter-07/go.mod
@@ -0,0 +1,17 @@
+module shoppinglist
+
+go 1.24.3
+
+require (
+	github.com/Masterminds/squirrel v1.5.4
+	github.com/mattn/go-sqlite3 v1.14.28
+	github.com/rs/cors v1.11.1
+	golang.org/x/crypto v0.40.0
+)
+
+require (
+	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
+	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
+	golang.org/x/net v0.41.0 // indirect
+	golang.org/x/text v0.27.0 // indirect
+)
---
+++ chapter-07/main.go
@@ -0,0 +1,306 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/rs/cors"
+	"golang.org/x/crypto/acme/autocert"
+)
+
+type ShoppingList struct {
+	ID    int      `json:"id"`
+	Name  string   `json:"name"`
+	Items []string `json:"items"`
+}
+
+type ShoppingListPatch struct {
+	Name  *string  `json:"name"`
+	Items []string `json:"items"`
+}
+
+type ListPushAction struct {
+	Item string `json:"item"`
+}
+
+type User struct {
+	Role     string
+	Username string
+	Password string
+}
+
+type Session struct {
+	Token    string
+	Expires  time.Time
+	Username string
+}
+
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+var (
+	allData  []ShoppingList = []ShoppingList{}
+	sessions                = map[string]*Session{}
+	allUsers                = map[string]*User{
+		"admin": {"admin", "admin", "password"},
+		"user":  {"user", "user", "password"},
+	}
+)
+
+var repository *Repository
+
+func main() {
+	var err error
+	repository, err = NewRepository("./database.db")
+	if err != nil {
+		fmt.Println("Unable to open the database:", err.Error())
+		os.Exit(1)
+	}
+	if err := repository.Init(); err != nil {
+		fmt.Println("Unable to initialize the database:", err.Error())
+		os.Exit(1)
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /lists", authRequired(handleListLists))
+	mux.HandleFunc("POST /lists", adminRequired(handleCreateList))
+	mux.HandleFunc("GET /lists/{id}", authRequired(handleGetList))
+	mux.HandleFunc("PUT /lists/{id}", adminRequired(handleUpdateList))
+	mux.HandleFunc("DELETE /lists/{id}", adminRequired(handleDeleteList))
+	mux.HandleFunc("PATCH /lists/{id}", adminRequired(handlePatchList))
+	mux.HandleFunc("POST /lists/{id}/push", adminRequired(handleListPush))
+	mux.HandleFunc("POST /login", handleLogin)
+
+	corsMiddleware := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://localhost:3000"},
+		AllowedMethods: []string{
+			http.MethodGet,
+			http.MethodPost,
+			http.MethodPut,
+			http.MethodPatch,
+			http.MethodDelete,
+			http.MethodOptions,
+		},
+		AllowedHeaders: []string{
+			"Content-Type",
+			"Authorization",
+		},
+		MaxAge: 300,
+	})
+
+	certManager := autocert.Manager{
+		Prompt:     autocert.AcceptTOS,
+		HostPolicy: autocert.HostWhitelist("ourdomain.com"),
+		Cache:      autocert.DirCache("certs"),
+	}
+
+	corsHandler := corsMiddleware.Handler(mux)
+
+	server := &http.Server{
+		Addr:      ":https",
+		Handler:   corsHandler,
+		TLSConfig: certManager.TLSConfig(),
+	}
+
+	go http.ListenAndServe(":http", certManager.HTTPHandler(nil))
+	server.ListenAndServeTLS("", "")
+}
+
+func handleCreateList(w http.ResponseWriter, r *http.Request) {
+	var list ShoppingList
+	err := json.NewDecoder(r.Body).Decode(&list)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	allData = append(allData, list)
+
+	w.WriteHeader(http.StatusCreated)
+
+	err = json.NewEncoder(w).Encode(list)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func handleListLists(w http.ResponseWriter, r *http.Request) {
+	data, err := json.Marshal(allData)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	_, err = w.Write(data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func handleDeleteList(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	for i, list := range allData {
+		if strconv.Itoa(list.ID) == id {
+			allData = append(allData[:i], allData[i+1:]...)
+
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+	}
+	http.Error(w, "List not found", http.StatusNotFound)
+}
+
+func handleUpdateList(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	for i, list := range allData {
+		if strconv.Itoa(list.ID) == id {
+			var updatedList ShoppingList
+			err := json.NewDecoder(r.Body).Decode(&updatedList)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			allData[i] = updatedList
+
+			if err := json.NewEncoder(w).Encode(updatedList); err != nil {
+
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+
+				return
+
+			}
+			return
+		}
+	}
+	http.Error(w, "List not found", http.StatusNotFound)
+}
+
+func handlePatchList(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	for i, list := range allData {
+		if strconv.Itoa(list.ID) == id {
+			var patch ShoppingListPatch
+			err := json.NewDecoder(r.Body).Decode(&patch)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			if patch.Name != nil {
+				list.Name = *patch.Name
+			}
+			if patch.Items != nil {
+				list.Items = patch.Items
+			}
+			allData[i] = list
+
+			err = json.NewEncoder(w).Encode(list)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			return
+		}
+	}
+	http.Error(w, "List not found", http.StatusNotFound)
+}
+
+func handleGetList(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	for _, list := range allData {
+		if strconv.Itoa(list.ID) == id {
+			data, err := json.Marshal(list)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			_, err = w.Write(data)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			return
+		}
+	}
+	http.Error(w, "List not found", http.StatusNotFound)
+}
+
+func handleListPush(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	for i, list := range allData {
+		if strconv.Itoa(list.ID) == id {
+			var item ListPushAction
+			err := json.NewDecoder(r.Body).Decode(&item)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			list.Items = append(list.Items, item.Item)
+			allData[i] = list
+
+			err = json.NewEncoder(w).Encode(list)
+			if err != nil {
+
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+
+				return
+
+			}
+			return
+		}
+	}
+	http.Error(w, "List not found", http.StatusNotFound)
+}
+
+func handleLogin(w http.ResponseWriter, r *http.Request) {
+	var data LoginRequest
+	json.NewDecoder(r.Body).Decode(&data)
+	user := allUsers[data.Username]
+	if user != nil && user.Password == data.Password {
+		token := strconv.Itoa(rand.Intn(100000000000))
+		sessions[token] = &Session{
+			Expires:  time.Now().Add(7 * 24 * time.Hour),
+			Username: user.Username,
+		}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{"token": token})
+	}
+	w.WriteHeader(http.StatusUnauthorized)
+}
+
+func authRequired(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		token := r.Header.Get("Authorization")
+		if !strings.HasPrefix(token, "Bearer ") {
+			http.Error(w, "unauthorized", http.StatusUnauthorized)
+			return
+		}
+		token = token[7:]
+		_, err := repository.GetSession(token)
+		if err != nil {
+			http.Error(w, "unauthorized", http.StatusUnauthorized)
+			return
+		}
+		next(w, r)
+	}
+}
+
+func adminRequired(next http.HandlerFunc) http.HandlerFunc {
+	return authRequired(func(w http.ResponseWriter, r *http.Request) {
+		token := r.Header.Get("Authorization")
+		token = token[7:]
+		user := allUsers[sessions[token].Username]
+		if user.Role != "admin" {
+			http.Error(w, "forbidden", http.StatusForbidden)
+			return
+		}
+		next(w, r)
+	})
+}
