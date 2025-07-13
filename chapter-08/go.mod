4682e630d78bdb4d77d7d0c16a0ce3a936ce8f8b
Jesús Espino <jesus@gitpod.io>
2025-07-13 12:03:49 +0200

    Adding chapter 8

---
+++ chapter-08/go.mod
@@ -0,0 +1,14 @@
+module shoppinglist
+
+go 1.24.3
+
+require (
+	github.com/Masterminds/squirrel v1.5.4
+	github.com/hashicorp/golang-lru/v2 v2.0.7
+	github.com/mattn/go-sqlite3 v1.14.28
+)
+
+require (
+	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
+	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
+)
---
+++ chapter-08/main.go
@@ -0,0 +1,294 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	lru "github.com/hashicorp/golang-lru/v2"
+)
+
+type ShoppingList struct {
+	ID    int      `json:"id"`
+	Name  string   `json:"name"`
+	Items []string `json:"items"`
+}
+
+type ShoppingListPatch struct {
+	Name  *string  `json:"name"`
+	Items []string `json:"items"`
+}
+
+type ListPushAction struct {
+	Item string `json:"item"`
+}
+
+type User struct {
+	Role     string
+	Username string
+	Password string
+}
+
+type Session struct {
+	Token    string
+	Expires  time.Time
+	Username string
+}
+
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+var (
+	allUsers = map[string]*User{
+		"admin": {"admin", "admin", "password"},
+		"user":  {"user", "user", "password"},
+	}
+	listsCache *lru.Cache[string, ShoppingList]
+)
+
+var repository *Repository
+
+func main() {
+	var err error
+	listsCache, err = lru.New[string, ShoppingList](128)
+	if err != nil {
+		fmt.Println("Unable to initialize the lists cache:", err.Error())
+		os.Exit(1)
+	}
+	repository, err = NewRepository("./database.db")
+	if err != nil {
+		fmt.Println("Unable to open the database:", err.Error())
+		os.Exit(1)
+	}
+	if err := repository.Init(); err != nil {
+		fmt.Println("Unable to initialize the database:", err.Error())
+		os.Exit(1)
+	}
+
+	http.HandleFunc("GET /lists", authRequired(handleListLists))
+	http.HandleFunc("POST /lists", adminRequired(handleCreateList))
+	http.HandleFunc("GET /lists/{id}", authRequired(handleGetList))
+	http.HandleFunc("PUT /lists/{id}", adminRequired(handleUpdateList))
+	http.HandleFunc("DELETE /lists/{id}", adminRequired(handleDeleteList))
+	http.HandleFunc("PATCH /lists/{id}", adminRequired(handlePatchList))
+	http.HandleFunc("POST /lists/{id}/push", adminRequired(handleListPush))
+	http.HandleFunc("POST /login", handleLogin)
+
+	fmt.Println("listening on port :8888")
+	http.ListenAndServe(":8888", nil)
+}
+
+func handleCreateList(w http.ResponseWriter, r *http.Request) {
+	var list ShoppingList
+	err := json.NewDecoder(r.Body).Decode(&list)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err = repository.CreateShoppingList(&list)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+
+	err = json.NewEncoder(w).Encode(list)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func handleListLists(w http.ResponseWriter, r *http.Request) {
+	lists, err := repository.GetAllShoppingLists()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	data, err := json.Marshal(lists)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	_, err = w.Write(data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func handleDeleteList(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	err := repository.DeleteShoppingList(id)
+	if err != nil {
+		http.Error(w, "List not found", http.StatusNotFound)
+		return
+	}
+	listsCache.Remove(id)
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func handleUpdateList(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	var updatedList ShoppingList
+	err := json.NewDecoder(r.Body).Decode(&updatedList)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err = repository.UpdateShoppingList(id, &updatedList)
+	if err != nil {
+		http.Error(w, "List not found", http.StatusNotFound)
+		return
+	}
+	listsCache.Remove(id)
+
+	if err := json.NewEncoder(w).Encode(updatedList); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func handlePatchList(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	var patch ShoppingListPatch
+	err := json.NewDecoder(r.Body).Decode(&patch)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = repository.PatchShoppingList(id, &patch)
+	if err != nil {
+		http.Error(w, "List not found", http.StatusNotFound)
+		return
+	}
+	listsCache.Remove(id)
+
+	list, err := repository.GetShoppingList(id)
+	if err != nil {
+		http.Error(w, "List not found", http.StatusNotFound)
+		return
+	}
+
+	err = json.NewEncoder(w).Encode(list)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func handleGetList(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	list, ok := listsCache.Get(id)
+	if !ok {
+		dbList, err := repository.GetShoppingList(id)
+		if err != nil {
+			http.Error(w, "List not found", http.StatusNotFound)
+			return
+		}
+		listsCache.Add(id, *dbList)
+	}
+
+	data, err := json.Marshal(list)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	_, err = w.Write(data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func handleListPush(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	var item ListPushAction
+	err := json.NewDecoder(r.Body).Decode(&item)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	list, err := repository.GetShoppingList(id)
+	if err != nil {
+		http.Error(w, "List not found", http.StatusNotFound)
+		return
+	}
+
+	list.Items = append(list.Items, item.Item)
+	err = repository.UpdateShoppingList(id, list)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	listsCache.Remove(id)
+
+	err = json.NewEncoder(w).Encode(list)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func handleLogin(w http.ResponseWriter, r *http.Request) {
+	var data LoginRequest
+	json.NewDecoder(r.Body).Decode(&data)
+	user := allUsers[data.Username]
+	if user != nil && user.Password == data.Password {
+		session, err := repository.AddSession(user.Username)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{"token": session.Token})
+		return
+	}
+	w.WriteHeader(http.StatusUnauthorized)
+}
+
+func authRequired(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		token := r.Header.Get("Authorization")
+		if !strings.HasPrefix(token, "Bearer ") {
+			http.Error(w, "unauthorized", http.StatusUnauthorized)
+			return
+		}
+		token = token[7:]
+		_, err := repository.GetSession(token)
+		if err != nil {
+			http.Error(w, "unauthorized", http.StatusUnauthorized)
+			return
+		}
+		next(w, r)
+	}
+}
+
+func adminRequired(next http.HandlerFunc) http.HandlerFunc {
+	return authRequired(func(w http.ResponseWriter, r *http.Request) {
+		token := r.Header.Get("Authorization")
+		token = token[7:]
+		session, err := repository.GetSession(token)
+		if err != nil {
+			http.Error(w, "unauthorized", http.StatusUnauthorized)
+			return
+		}
+		user := allUsers[session.Username]
+		if user.Role != "admin" {
+			http.Error(w, "forbidden", http.StatusForbidden)
+			return
+		}
+		next(w, r)
+	})
+}
